80d5f49ef58ef5d305f2cd520909becc33326bc9
MikelShifrin <miki.super@gmail.com>
2019-10-04 15:39:27 -0400

    testing out Abstract class definition

--- Practice/src/soen387/AbstractClassExample.java
+++ Practice/src/soen387/AbstractClassExample.java
@@ -1,6 +1,20 @@
 package soen387;
 
 public abstract class AbstractClassExample {
+	private String name;
+	private String gender;
+	
+	public AbstractClassExample(String name, String gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+	public String toString() {
+		return "Name: " + this.name + " Gender: " + this.gender;
+	}
+	public void changeName(String name) {
+		this.name = name;
+	}
+	
 	public abstract double getPay();
 	public abstract void doIt(int count);
 	
